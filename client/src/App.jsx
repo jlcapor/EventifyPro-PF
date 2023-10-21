b5576719bceece65d4f333f2da660b1531c7d631
Jose Luis Capote <capoterojas123@gmail.com>
2023-10-21 00:43:10 -0500

    Estructura del layouts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-// import './App.css'
 import { Routes, Route } from "react-router-dom";
+import './App.css'
 function App() {
 
   return (
---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchBar = ({}) => {
+	return <div>SearchBar</div>;
+};
+
+SearchBar.propTypes = {};
+
+export default SearchBar;
