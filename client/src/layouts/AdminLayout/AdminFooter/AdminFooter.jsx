d4951e951cf4b19d3f0729e5890ca63bd6241480
Jose Luis Capote <capoterojas123@gmail.com>
2023-10-21 00:50:51 -0500

    Estructura del layout admin

---
+++ client/src/layouts/AdminLayout/AdminFooter/AdminFooter.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AdminFooter = ({}) => {
+	return <div>AdminFooter</div>;
+};
+
+
+export default AdminFooter;
---
+++ client/src/layouts/AdminLayout/AdminNavbar/AdminNavbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AdminNavbar = ({}) => {
+	return <div>AdminNavbar</div>;
+};
+
+
+export default AdminNavbar;
