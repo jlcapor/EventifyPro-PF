d52c71a4447d573eff35f271ba077d7dd68358a7
Jose Luis Capote <capoterojas123@gmail.com>
2023-10-22 14:09:02 -0500

    confirmacion de cuenta

--- client/src/pages/Auth/ConfirmAccount.jsx
+++ client/src/pages/Auth/ConfirmAccount.jsx
@@ -1,9 +1,10 @@
 const ConfirmAccount = () => {
-  return (
+return (
     <div>
-      <h1>
-        Please confirm your account by clicking on the link sent to your email. If you do not receive an email, please contact us for assistance.
-      </h1>
+        <h1>
+            Please confirm your account by clicking on the link sent to your email. If you do not receive an email, please
+            contact us for assistance.
+        </h1>
     </div>
   );
 }
