cd17742e06fb1f9f591cedce4f43d25d30dec54e
Jose Luis Capote <capoterojas123@gmail.com>
2023-10-22 20:37:13 -0500

    creacion de proyecto con nextjs

--- client/src/pages/Auth/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ConfirmAccount } from './ConfirmAccount';
-export { default as ForgotPassword } from './ForgotPassword';
-export { default as Login } from './Login';
-export { default as NewPassword } from './NewPassword';
-export { default as Register } from './Register';
