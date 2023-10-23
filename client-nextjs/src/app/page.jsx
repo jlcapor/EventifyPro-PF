930677ee6a142c4697572424483ccc30594c1f53
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2023-10-23 18:34:34 -0500

    tailwind css page

--- client-nextjs/src/app/page.jsx
+++ client-nextjs/src/app/page.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <Container>
-      <div  className=" pt-24 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6gap-8">
-     sddfgdfgdf
+      <div className="pt-24 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6gap-8">
+        
       </div>
     </Container>
    
