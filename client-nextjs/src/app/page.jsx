345e4a9f69ca121ffb739801bc6cef97e3c070a5
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2023-10-23 18:30:49 -0500

    talwindcss

---
+++ client-nextjs/src/app/page.jsx
@@ -0,0 +1,13 @@
+import Container from '@/components/Container'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Container>
+      <div  className=" pt-24 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6gap-8">
+     sddfgdfgdf
+      </div>
+    </Container>
+   
+  )
+}
