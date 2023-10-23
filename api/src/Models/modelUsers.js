c67d3b17751562681b228dc72bc246320e9ef411
iyas89 <iyasasaad@gmail.com>
2023-10-23 13:10:36 -0300

    modelUsers

---
+++ api/src/Models/modelUsers.js
@@ -0,0 +1,31 @@
+const {DataTypes} = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define('Users', {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue : DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        telefono: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        } 
+})
+}
