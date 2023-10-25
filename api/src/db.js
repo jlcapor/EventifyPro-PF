bfadbef78d102c946fd821b163c4e315c5b05379
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2023-10-24 23:09:31 -0500

    Relaciones de modelos

--- api/src/db.js
+++ api/src/db.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const ModelAdmin = require('./models/Admin');
+const ModelEvent= require('./models/Event');
+const ModelEventType= require('./models/EventType');
+const ModelTicket= require('./models/Ticket');
+const ModelUser= require('./models/User');
+
+
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BDD} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, native: false, });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${BDD}`, {
+  logging: false, native: false});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -24,9 +29,24 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
+// DEFINICION DE MODELOS A USAR
+ModelAdmin(sequelize);
+ModelEvent(sequelize);
+ModelEventType(sequelize);
+ModelTicket(sequelize);
+ModelUser(sequelize);
 
+const {Users, Events, EventTypes, Tickets} = sequelize.models;
 
-const { } = sequelize.models;
+EventTypes.hasMany(Events);
+Events.belongsTo(EventTypes);
+
+Events.hasMany(Tickets);
+Tickets.belongsTo(Events);
+
+Users.hasMany(Tickets);
+Tickets.belongsTo(Users);
+
 
 // RELACIONES
 
