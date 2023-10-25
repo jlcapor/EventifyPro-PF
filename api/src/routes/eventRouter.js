ba8f2f0e84bdf5dcdfea446620e1ebb31dd7f4dc
FVidalYepz <Frananyelvidalyepez@gmail.com>
2023-10-25 18:35:58 -0400

    GetDetailById

--- api/src/routes/eventRouter.js
+++ api/src/routes/eventRouter.js
@@ -1,8 +1,10 @@
-const {Router} = require("express");
-const {getEventsHandler} = require("../handlers/getEventsHandler");
+const { Router } = require("express");
+const { getEventsHandler } = require("../handlers/getEventsHandler");
+const { getDetailByIdHandler } = require("../handlers/getDetailByIdHandler");
 
 const eventRouter = Router();
 
 eventRouter.get("/", getEventsHandler);
+eventRouter.get("/:id", getDetailByIdHandler);
 
 module.exports = eventRouter;
