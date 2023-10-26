d489c9b12cc810d2a77893342d3b2afb2c32da2f
FOteroRimini <facurimini@gmail.com>
2023-10-26 15:54:45 -0300

    await

--- api/src/handlers/getEventsHandler.js
+++ api/src/handlers/getEventsHandler.js
@@ -3,7 +3,7 @@ const { getEvents,getEventsByName } = require("../controllers/getEvents");
 const getEventsHandler = async (req, res) => {
   const {name} = req.query;
   try {
-    const data = name ? getEventsByName(name) : await getEvents();
+    const data = name ? await getEventsByName(name) : await getEvents();
     res.status(201).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
