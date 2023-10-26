db90b98456b352e19253c94524d9b650b656f71b
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2023-10-25 19:36:50 -0500

    Eliminaron el component EventCards

--- client-nextjs/src/components/EventCards/EventCards.jsx
+++ client-nextjs/src/components/EventCards/EventCards.jsx
@@ -1,13 +1,13 @@
 'use client';
 import EventCard from "../EventCard/EventCard";
 const EventCards = ({events}) => {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {
-                events.map((event)=> <EventCard key={event.id} event={event} />)
-            }
-        </div>
-    );
+	return (
+		<div className="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10">
+			{
+				events.map((event)=> <EventCard key={event.id} event={event} />)
+			}
+		</div>
+	);
 };
 
 export default EventCards;
