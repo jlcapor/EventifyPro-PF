d9716e028b4fdbe415734f7307123f7d863bc110
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2023-10-26 19:19:20 -0500

    Delete api/src/routes/eventTypeRouter.js

--- api/src/routes/eventTypeRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getEventTypesHandler } = require("../handlers/getEventTypesHandler");
-
-
-const eventTypeRouter = Router();
-
-eventTypeRouter.get("/", getEventTypesHandler);
-
-module.exports = eventTypeRouter;
